820c243f6fd9ed2e1ee223f128cc4da9959f32cc
Jose Carlos Hernandez <jose.hernandez@line-up.com.mx>
2025-05-07 22:13:27 -0600

    se quita health

--- blacklist/src/routes.py
+++ blacklist/src/routes.py
@@ -29,7 +29,7 @@ def token_required(f):
 
 @api_bp.route('/health', methods=['GET'])
 def health_check():
-    return jsonify({"status": "OK"}), 200
+    return jsonify({"status": "OK"}), 404
 
 class BlacklistResource(Resource):
 
